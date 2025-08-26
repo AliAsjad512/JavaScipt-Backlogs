d02e9aec3aa786937ad953a61e8c135b18ec5c45
AliAsjad512 <aa0504503@gmail.com>
2025-08-26 18:15:34 -0400

    iteration over keys

--- Javascript/Object/Quokka.js
+++ Javascript/Object/Quokka.js
@@ -18,24 +18,65 @@
 // }
 // console.log(person.address)
 
-
 // const remote ={
 //     color: 'black',
 //     brand: 'xyx',
 //     dimensions : { height:1, width:1}
 // }
 
+// const courses = {
+//   170: {
+//     title: "Introduction to Programming",
+//     description: "Develop algorithms for computers",
+//     creditHours: 5,
+//   },
+//   250: {
+//     title: "Web Development",
+//     description: "Buid web applications",
+//     creditHours: 4,
+//   },
+//   310: {
+//     title: "Operating Systmes",
+//     description: "Process management and memory management",
+//     creditHours: 3,
+//   },
+//   430: {
+//     title: "Articial Intelligence",
+//     description: "Simualate human thinking",
+//     creditHours: 2,
+//   },
+// };
 
-const courses ={
-    "170" : { title: "Introduction to Programming", description:"Develop algorithms for computers", creditHours:5},
-     "250" : { title: "Web Development", description:"Buid web applications", creditHours:4},
-     "310" : { title: "Operating Systmes", description:"Process management and memory management", creditHours:3},
-      "430" : { title: "Articial Intelligence", description:"Simualate human thinking", creditHours:2}
-}
+// let keys = Object.keys(courses);
 
-for (let key in courses) {
-if(courses[key].creditHours > 3){
+// for (let key in courses) {
+//   console.log(key);
+// }
 
-  console.log(`${key}: ${courses[key].title}`);
-}
-}
+
+const courses = {
+  170: {
+    title: "Introduction to Programming",
+    description: "Develop algorithms for computers",
+    creditHours: 5,
+  },
+  250: {
+    title: "Web Development",
+    description: "Buid web applications",
+    creditHours: 4,
+  },
+  310: {
+    title: "Operating Systmes",
+    description: "Process management and memory management",
+    creditHours: 3,
+  },
+  430: {
+    title: "Articial Intelligence",
+    description: "Simualate human thinking",
+    creditHours: 2,
+  },
+};
+
+let keys = Object.keys(courses);
+
+keys.forEach(key=>console.log(key));
