8e40fdd3d8279bcec24cc64a535764777f9ae872
AliAsjad512 <aa0504503@gmail.com>
2025-09-12 22:10:40 -0400

    new

--- Javascript/DOM/DOMz/Earlyprogramming/index.js
+++ Javascript/DOM/DOMz/Earlyprogramming/index.js
@@ -1,2 +1,8 @@
  let para = document.getElementById("early_languages");
  let listItems = document.querySelectorAll("li");
+
+ 
+
+ for(let i=0;i<listItems.length;i++){
+    console.log(listItems[i].textContent)
+ }
